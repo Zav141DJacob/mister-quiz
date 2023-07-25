fc200614c89f5ac8cbcffedf9d86fa1c9d7b3396
Jacob <jaagup.tomingas@gmail.com>
2023-07-25 03:02:25 +0300

    hopefully final fix for start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-php artisan sail:install
 sudo service apache2 stop
+composer install
+php artisan sail:install
 php -r "copy('.env.example', '.env');"
-vendor/bin/sail up -d
 
 while IFS="" read -r p || [ -n "$p" ]
 do
   if printf '%s' "$p" | grep -Eq '^APP_KEY'; then
-    key=$(vendor/bin/sail artisan key:generate --show)
+    key=$(php artisan key:generate --show)
     echo "APP_KEY=$key" >> .envv
     printf '%s\n' "APP_KEY generated and set"
   else
@@ -19,4 +19,7 @@ done < .env
 cp .envv .env
 rm .envv
 
-vendor/bin/sail artisan migrate:fresh --seed
+./vendor/bin/sail up -d
+
+
+./vendor/bin/sail artisan migrate:fresh --seed
